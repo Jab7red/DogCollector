e6044411952b1cdbb2ba4164b11d55b1c6382f4b
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-02-16 21:48:49 -0600

    add photo upload feature

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Dog, Toy, Feeding
+from .models import Dog, Toy, Feeding, Photo
 
 admin.site.register(Dog)
 admin.site.register(Toy)
 admin.site.register(Feeding)
+admin.site.register(Photo)
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog, Toy, Feeding
 from .forms import FeedingForm
+import uuid
+import boto3
+from .models import Dog, Toy, Photo
+
+S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
+BUCKET = 'cat-collector-jb'
 
 # Create your views here.
 def home(request):
@@ -44,6 +49,23 @@ def add_feeding(request, dog_id):
  
 def assoc_toy(request, dog_id, toy_id):
     Dog.objects.get(id=dog_id).toys.add(toy_id)
+    return redirect('dog_detail', dog_id=dog_id)
+
+
+
+def add_photo(request, dog_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Photo(url=url, dog_id=dog_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
     return redirect('dog_detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
