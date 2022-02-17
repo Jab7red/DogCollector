aaada358a8eb440992c1715ef610b82be80e30fd
Jared Bradley <jaredbradley@jareds-mbp.lan>
2022-02-16 20:03:46 -0600

    dog model UX update

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -16,5 +17,17 @@ def dogs_index(request):
 def dog_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', { 'dog': dog })
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    fields = '__all__'
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
 
 
